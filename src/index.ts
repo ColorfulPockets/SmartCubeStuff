f96fd26fe12db04d64105096d4a5cd3dcfbf3ff8
Andrew Nathenson <ColorfulPockets01@gmail.com>
2020-07-02 17:59:03 -0700

    basics setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hi")
+import { parse } from "cubing/alg";
+import { connect, BluetoothPuzzle, MoveEvent, OrientationEvent } from "cubing/bluetooth";
+
+document.querySelector("#connect").addEventListener("click", async () => {
+    console.log(parse("[R, U]"))
+    const puzzle: BluetoothPuzzle = await connect()
+    puzzle.addMoveListener( (e: MoveEvent) => {
+        console.log(e.latestMove)
+    })
+    puzzle.addOrientationListener( (e: OrientationEvent) => {
+        console.log(e.quaternion)
+    } )
+})
