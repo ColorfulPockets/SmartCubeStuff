184a8cd952eb20363895d3a4b39ebd338271f37e
Lucas Garron <code@garron.net>
2020-07-03 01:13:14 -0700

    Update `cubing.js` again to simplify vantage position and orientation.

--- src/app.ts
+++ src/app.ts
@@ -14,23 +14,15 @@ export class BluetoothApp {
     setup() {
         document.querySelector("#connect").addEventListener("click", this.onConnectButtonPress.bind(this))
 
-        this.twisty = new TwistyPlayer({ 
-            alg: parse("")
+        this.twisty = new TwistyPlayer({
+            playerConfig: {
+                experimentalCube3DViewConfig: {
+                    experimentalInitialVantagePosition: new Vector3(0, 4, 4)
+                }
+            }
         })
         
         document.body.appendChild(this.twisty)
-
-        setTimeout(this.hackInitialOrientation.bind(this), 100)
-    }
-
-    hackInitialOrientation() {
-        const mainVantage = this.twisty.experimentalGetPlayer()
-        .cube3DView.experimentalGetCube3D()
-        .experimentalGetVantages()[0]
-
-        mainVantage.camera.position.set(0,4,4)
-        mainVantage.camera.lookAt(new Vector3(0,0,0))
-        mainVantage.render()
     }
 
     async onConnectButtonPress() {
@@ -46,18 +38,6 @@ export class BluetoothApp {
     }
 
     async onOrient(e: OrientationEvent): Promise<void> {
-        const { x, y, z, w } = e.quaternion;
-        const quat = new Quaternion(x, y, z, w);
-        await this.setOrientation(quat)
-    }
-
-    async setOrientation(quat: Quaternion) {
-        this.twisty
-        .experimentalGetPlayer()
-        .cube3DView.experimentalGetCube3D()
-        .experimentalGetCube()
-        .quaternion.copy(quat);
-    
-        this.twisty.experimentalGetPlayer().cube3DView.experimentalGetCube3D().experimentalGetVantages()[0].render()
+        this.twisty.experimentalGetPlayer().cube3DView.experimentalSetOrientation(e.quaternion);
     }
 }
