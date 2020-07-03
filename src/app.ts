b0716dbf79debb38eaabab58ab956259e69f388c
Andrew Nathenson <ColorfulPockets01@gmail.com>
2020-07-02 18:43:20 -0700

    cube drawing working

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,49 @@
 import { parse } from "cubing/alg";
 import { connect, BluetoothPuzzle, MoveEvent, OrientationEvent } from "cubing/bluetooth";
+import { Twisty } from "cubing/twisty";
+import { Quaternion } from "three";
 
 export class BluetoothApp {
+    private twisty: Twisty
+
     constructor() {
         this.setup()
     }
 
     setup() {
         document.querySelector("#connect").addEventListener("click", this.onConnectButtonPress.bind(this))
+
+        // TODO update to new syntax
+        const twistyElem = document.createElement("twisty")
+        this.twisty = new Twisty(twistyElem, {alg: parse("")})
+        document.body.appendChild(twistyElem)
     }
 
     async onConnectButtonPress() {
         console.log(parse("[R, U]"))
         const puzzle: BluetoothPuzzle = await connect()
         puzzle.addMoveListener(this.onMove.bind(this))
-        // puzzle.addOrientationListener( (e: OrientationEvent) => {
-        //     console.log(e.quaternion)
-        // } )
+        puzzle.addOrientationListener(this.onOrient.bind(this))
     }
 
     async onMove(e: MoveEvent): Promise<void> {
         console.log(e.latestMove)
+        this.twisty.experimentalAddMove(e.latestMove)
+    }
+
+    async onOrient(e: OrientationEvent): Promise<void> {
+        const { x, y, z, w } = e.quaternion;
+        const quat = new Quaternion(x, y, z, w);
+        await this.setOrientation(quat)
+    }
+
+    async setOrientation(quat: Quaternion) {
+        this.twisty
+        .experimentalGetPlayer()
+        .cube3DView.experimentalGetCube3D()
+        .experimentalGetCube()
+        .quaternion.copy(quat);
+    
+        this.twisty.experimentalGetPlayer().cube3DView.experimentalGetCube3D().experimentalGetVantages()[0].render()
     }
 }
